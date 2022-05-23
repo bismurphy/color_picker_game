ca7bfe92f78c99341a7430612898a8709e776605
Joey Murphy <tjmurphy@mit.edu>
2022-05-23 12:27:57 -0400

    Fixed some badly ingested color names

--- parse_colors.py
+++ parse_colors.py
@@ -3,6 +3,9 @@ with open("color_dataset") as f:
     outlines = ""
     for i in range(0,len(colorlines),4):
         name = colorlines[i][:-1]
+        for c in name:
+            if c != " " and not c.isalpha():
+                print(name)
         red = colorlines[i+1][:-1]
         green = colorlines[i+2][:-1]
         blue = colorlines[i+3][:-1]
